e798ab5f1ef11dc90c6670b3a636574704dd16ca
DJ <dtaylor88@gmail.com>
2021-11-19 15:03:14 -0800

    add links to home/about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const about = () => {
     return (
         <div>
            <h1>About This Site</h1> 
+           <Link to='/'>Back to Home</Link>
         </div>
     );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'gatsby';
+
 
 const index = () => {
     return (
         <div>
             <h1>Hello Frontend Masters!</h1>
+            <Link to='/about'>About</Link>
         </div>
     );
 };
