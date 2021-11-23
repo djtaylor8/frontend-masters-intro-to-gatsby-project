e5b2bce00f0f649261271bf77a1332f43e32b370
DJ <dtaylor88@gmail.com>
2021-11-23 09:56:36 -0800

    add layout skeleton component

---
+++ src/components/layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import { Seo } from './seo';
+
+const layout = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default layout;
