d88815317e7996c4a86615ae24043b4330a3f58f
DJ <dtaylor88@gmail.com>
2021-11-23 00:05:54 -0800

    adjust seo for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { Seo } from '../components/seo';
 
 const about = () => {
     return (
+        <>
+        <Seo 
+            title='About This Site'
+            description='More info about this site'
+        />
         <div>
            <h1>About This Site</h1> 
            <Link to='/'>Back to Home</Link>
         </div>
+        </>
     );
 };
 
